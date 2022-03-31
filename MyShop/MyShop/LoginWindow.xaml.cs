95eb7cc3d20f02570c17bc11cb5fd340ad97c7b0
tranthetoan7501 <68367691+tranthetoan7501@users.noreply.github.com>
2022-03-31 23:22:06 +0700

    login with hash pass

--- MyShop/MyShop/LoginWindow.xaml.cs
+++ MyShop/MyShop/LoginWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -13,14 +15,25 @@ using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.Diagnostics;
 
 namespace MyShop
 {
     /// <summary>
     /// Interaction logic for LoginWindow.xaml
     /// </summary>
+    /// 
+    class TaiKhoan
+    {
+        public string id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+    }
     public partial class LoginWindow : Window
     {
+        
         private const int GWL_STYLE = -16;
         private const int WS_SYSMENU = 0x80000;
         [DllImport("user32.dll", SetLastError = true)]
@@ -40,7 +53,48 @@ namespace MyShop
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (userNameTextBox.Text == "admin" && passWordBox.Password == "qwe3@1")
+
+            var connectionString =
+                "Server=.\\sqlexpress;Database=MyShop;Trusted_Connection=True;";
+
+            // Kết nối
+            var connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            // Sau khi kết nối thành công
+            var sql = "select * from TaiKhoan";
+            var command = new SqlCommand(sql, connection);
+
+            var reader = command.ExecuteReader();
+
+            List<TaiKhoan> taikhaons = new List<TaiKhoan>();
+
+            while (reader.Read())
+            {
+                var _id = (string)reader["TaiKhoan_id"];
+                var _name = (string)reader["TaiKhoan_username"];
+                var _password = (string)reader["TaiKhoan_password"];
+
+                TaiKhoan taikhoan = new TaiKhoan()
+                {
+                    id = _id,
+                    username = _name,
+                    password = _password
+                };
+                taikhaons.Add(taikhoan);
+            }
+            string hashpass = hashPassword(passWordBox.Password);
+            Debug.WriteLine(hashpass);
+
+            if (check_account(userNameTextBox.Text, hashpass, taikhaons))
             {
                 DialogResult = true;
             }
@@ -51,5 +105,108 @@ namespace MyShop
                 passWordBox.Password = "";
             }
         }
+
+
+        private bool check_account(string username,string password, List<TaiKhoan> tk)
+        {
+            for(int i = 0; i < tk.Count; i++)
+            {
+                if (tk[i].username == username && tk[i].password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var connectionString =
+                "Server=.\\sqlexpress;Database=MyShop;Trusted_Connection=True;";
+
+            // Kết nối
+            var connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            // Sau khi kết nối thành công
+            var sql = "select * from TaiKhoan";
+            var command = new SqlCommand(sql, connection);
+
+            var reader = command.ExecuteReader();
+
+            List<TaiKhoan> taikhaons = new List<TaiKhoan>();
+
+            while (reader.Read())
+            {
+                var _id = (string)reader["TaiKhoan_id"];
+                var _name = (string)reader["TaiKhoan_username"];
+                var _password = (string)reader["TaiKhoan_password"];
+
+                TaiKhoan taikhoan = new TaiKhoan()
+                {
+                    id = _id,
+                    username = _name,
+                    password = _password
+                };
+                taikhaons.Add(taikhoan);
+            }
+
+
+            if (username_is_not_exit(userNameTextBox.Text, taikhaons) && passWordBox.Password!="")
+            {
+                string statement = "INSERT INTO TaiKhoan VALUES (@id,@name,@pass)";
+                
+                SqlCommand cmd = new SqlCommand(statement, connection);
+                cmd.CommandType = CommandType.Text;
+                
+                cmd.Parameters.AddWithValue("@id", taikhaons.Count+"");
+                
+                cmd.Parameters.AddWithValue("@name", userNameTextBox.Text);
+                
+                cmd.Parameters.AddWithValue("@pass", passWordBox.Password);
+                
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                
+
+
+                DialogResult = true;
+            }
+            else
+            {
+                textBlock.Text = "Password or UserName is wrong!";
+                userNameTextBox.Text = "";
+                passWordBox.Password = "";
+            }
+        }
+
+        private bool username_is_not_exit(string username, List<TaiKhoan> tk)
+        {
+            for (int i = 0; i < tk.Count; i++)
+            {
+                if (tk[i].username == username || username=="")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private string hashPassword(string pass)
+        {
+            SHA1CryptoServiceProvider sHA1 = new SHA1CryptoServiceProvider();
+            byte[] password_byte = Encoding.ASCII.GetBytes(pass);
+            byte[] salt_byte = sHA1.ComputeHash(password_byte);
+            MessageBox.Show(Convert.ToBase64String(salt_byte));
+            return Convert.ToBase64String(salt_byte);
+        }
+
     }
 }
